c99de1e9cbca7c1576b70259a5126833da7be984
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-13 17:44:47 -0700

    Second Commit

--- gif-search/app.js
+++ gif-search/app.js
@@ -1,24 +1,51 @@
-// var express = require('express');
-// var app = express();
-//
-// app.listen(3000, function () {
-//   console.log('Gif Search listening on port localhost:3000!');
-// });
 
 // app.js
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 
+// REQUIRE HTTP MODULE
+var http = require('http');
+var giphy = require('giphy-api')();
+
+app.use(express.static('public'));
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
+
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  });
 });
 
 
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  http.get(url, function(response) {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+     body += d;
+    });
+
+    response.on('end', function() {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      var parsed = JSON.parse(body);
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', {gifs: parsed.data})
+    });
   });
+})
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'https://i.giphy.com/media/l41m1fsyxDSRgjqMM/giphy.webp';
+  res.render('hello-gif', {gifUrl: gifUrl});
+});
+
+
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
+)}
